3d45713636671fd231e8f8c901f7768a3c5618c2
nicol-buratti <nickibrt8@gmail.com>
2025-08-18 18:54:11 +0200

    removed unused files

--- merge_csv.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df1 = pd.read_csv("U01_R01_accel.csv")
-df2 = pd.read_csv("U01_R01_gyro.csv")
-
-df1 = pd.concat([df1, df2], axis=1)
-
-
-df1.to_csv("merged.csv", index=False)
