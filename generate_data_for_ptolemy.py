90a68791b9d00d385bf81bb4135d8897224d378f
nicol-buratti <nickibrt8@gmail.com>
2025-09-09 13:49:58 +0200

    handled exception

--- generate_data_for_ptolemy.py
+++ generate_data_for_ptolemy.py
@@ -1,12 +1,15 @@
 import pandas as pd
+import sys
 
 try:
-	df1 = pd.read_csv('raw_data/F02_U09_R01_data.csv')
+    df1 = pd.read_csv("raw_data/F02_U09_R01_data.csv")
 except FileNotFoundError:
-	raise FileNotFoundError('Run generate_raw_data_csv.py first')
+    print("Run generate_raw_data_csv.py first")
+    sys.exit(1)
 
-df_adl = df1[df1['label'] == 'ADL']
+df_adl = df1[df1["label"] == "ADL"]
 l = [df_adl.copy()] * 100
 
 df_bho = pd.concat([df1] + l, axis=0).dropna().reset_index(drop=True)
-df_bho.to_csv('ADL_fall_raw_signal.csv', index=False)
+df_bho.to_csv("ADL_fall_raw_signal.csv", index=False)
+print("Generated ADL_fall_raw_signal.csv")
